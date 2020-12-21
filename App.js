b90df57446dff49f35c4ec8af45bbcc6fa7cf179
saanhvisrivastava <63591636+saanhvisrivastava@users.noreply.github.com>
2020-12-21 20:02:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends React.Component{
+render(){
+  return(
+   <WelcomeScreen/>
+
+  )
+}
+
+}
